8a37853a8f2dbe9f0b2bf9264ec68dc51e938da7
vatshariyani <vatshariyani16@gmail.com>
2024-01-27 02:18:47 +0530

    Added Login Activity

---
+++ app/src/main/java/com/example/a4you/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a4you
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Additional initialization code for the login activity can be added here
+    }
+}
