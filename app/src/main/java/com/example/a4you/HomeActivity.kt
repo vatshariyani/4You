982a8e0a51a5f7efd7fd0c205b1af24c3d3ee0a4
vatshariyani <vatshariyani16@gmail.com>
2024-01-28 16:12:14 +0530

    Added Home Activity

---
+++ app/src/main/java/com/example/a4you/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a4you
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class HomeActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
