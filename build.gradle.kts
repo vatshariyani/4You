c37e9e538d9f5694d3779505d556b92b3dd8df23
ranaaapporv <rana.apoorv03@gmail.com>
2024-02-02 12:51:41 +0530

    Added MongoDB dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id ("io.realm.kotlin") version "1.11.0" apply false
 }
