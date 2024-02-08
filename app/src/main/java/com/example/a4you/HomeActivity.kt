08952094b17f9800efd32d7c4b20d63a75fcfba8
vatshariyani <vatshariyani16@gmail.com>
2024-02-08 13:31:35 +0530

    Added Username

--- app/src/main/java/com/example/a4you/HomeActivity.kt
+++ app/src/main/java/com/example/a4you/HomeActivity.kt
@@ -2,12 +2,25 @@ package com.example.a4you
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.example.a4you.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : ComponentActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            val email = currentUser.email
+            val userName = currentUser.displayName
+
+            binding.txtEmail.text = email
+            binding.txtUsername.text = userName
+        }
     }
 }
