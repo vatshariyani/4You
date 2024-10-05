6d957fe2304da9d24562b898d13df1ea919aa810
Vats Hariyani <62383230+vatshariyani@users.noreply.github.com>
2024-10-05 22:05:46 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,16 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+<<<<<<< HEAD
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.android.material:material:1.11.0")
+=======
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
+>>>>>>> parent of c0c553b (Revert "Update Dependencies")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
